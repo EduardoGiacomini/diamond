77091267efcee8964f5aa5a25431417f1719c3a8
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-01-03 14:05:32 -0300

    Deploy to gh-pages 1.0

--- README.md
+++
@@ -1,3 +0,0 @@
-# Diamond Landing Page
-
-A simple and beautiful landing page for Diamond (fictional company).
--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
- 
-gulp.task('default', () =>
-    gulp.src('assets/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('assets'))
-);
